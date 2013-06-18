8592eff6bb41d1da0a6b97d00178d27f987a9c65
sunztuII <c13joshua.christman@gmail.com>
2013-06-18 14:06:05 -0600

    Added the initial files

---
+++ nexpect.py
@@ -0,0 +1,176 @@
+'''
+nexpect.py
+Authors: Josh Christman and Nathan Hart
+Version: 1.0
+
+Changelog (v1.0):
+    - Added lots of comments
+    - Added a spawn method
+    - Changed the class name from SocketInteract to nexpect (Socket Expect)
+    - Added ability to pass a tuple for connection information instead of a socket object
+    - Added a timeout to the class that defaults to 30 seconds.
+    - Changed the function name "sendLine" to "sendline" to match pexpect function name
+    - Changed the expect method to return only data instead of tuple if a list of regexes was not passed into function
+    - Added timeout code to the expect method that will correctly change the socket timeout value based on how much time is left in class timeout
+    - Fixed the regex for loop so that it actually works
+    - Added optional parameters to expect method
+        - recvsize: changes the size of each socket receive in the while loop
+        - timeout: a local override to the class wide timeout if user wants a specific timeout on a specific call to expect
+    - Added function nexpect.settimeout()
+    - Added shutdown and start methods
+'''
+
+import threading,sys,socket,re,time
+
+def spawn(sock, timeout=30):
+    return nexpect(sock, timeout=timeout)
+
+'''
+The class nexpect is a socket expect module written using basic python modules. It should work on any
+system with Python 2.7
+'''
+class nexpect():
+    '''
+    The constructor has one mandatory parameter:
+        sock - This can be either a tuple with an address and port to connect to, or a socket object.
+        Ex: s = nexpect(('www.example.com',1234))
+        
+            s = nexpect(('123.123.123.123',1234))
+
+            sock = socket.socket()
+            sock.connect(('www.example.com',1234))
+            s = nexpect(sock)
+
+    Optional parameters are:
+        timeout - Sets the class timeout variable used as the timeout for the expect method
+    '''
+    def __init__(self, sock, timeout=30):
+        self.timeout = timeout
+        if type(sock) == type(()):
+            self.socket = socket.socket()
+            self.socket.connect(sock)
+        elif type(sock) == type(socket.socket()):
+            self.socket = sock
+        else:
+            raise TypeError
+
+    '''
+    This method does nothing but call the send method of the socket and pass the data to the socket
+    '''
+    def send(self, data):
+        self.socket.sendall(data)
+
+    '''
+    This method appends a delimeter to the data and then sends it to the socket
+
+    Optional parameters are:
+        delimeter - Defaults to a '\n' but can be set to anything
+    '''
+    def sendline(self, data, delimeter='\n'):
+        self.socket.sendall(data + delimeter)
+
+    '''
+    This function takes a single regex in string form or a list/tuple of string regexes and receives data
+    on the socket until it matches the regex. If a single regex is passed in, only the data received is
+    returned from the function. If a list/tuple of regexes is passed in, the function returns a tuple of
+    the data and the index of the regex it matched.
+
+    Optional parameters are:
+        recvsize - the size of the data to receive each time through the loop. It defaults to 1 but can
+        be slow this way. Increase this if you know that the data being sent to you will be fairly regular.
+
+        timeout - a local timeout override to the class variable timeout. This can be used for a time when
+        you want a different timeout than the normal.
+    '''
+    def expect(self, regex, recvsize=1, timeout=0):
+        isList = False
+        if type(regex) == type(()) or type(regex) == type([]):
+            isList = True
+
+        data = ''
+        t0 = time.time()
+        while True:
+            t1 = time.time()
+            elapsedTime = t1-t0                 # Get the elapsed time since before the receive loop started
+            if not timeout == 0:                # If there is a local timeout override as function parama=eter
+                if elapsedTime > timeout:       # Test that instead
+                    raise TimeoutException()
+                else:    # If it hasn't timed out, set the socket's timeout so that it won't block forever
+                    self.socket.settimeout(timeout - elapsedTime)
+            elif elapsedTime > self.timeout:    # Else test the object's timeout
+                raise TimeoutException()
+            else:   # If it hasn't timed out, set the socket's timeout so that it won't block forever
+                self.socket.settimeout(self.timeout - elapsedTime)
+
+            try:
+                data += self.socket.recv(recvsize)  # Receive bytes
+            except:
+                raise TimeoutException()
+            
+            if isList:                                  # Check if a list or tuple of regexes was passed in
+                for counter,reg in enumerate(regex):    # Enumerate the regexes for testing
+                    if re.search(reg, data):
+                        return data, counter            # Return the data and the index of the regex found
+            else:
+                if re.search(regex, data):              # If only a single regex was passed in, return the data if it is found
+                    return data
+
+    '''
+    The interact method makes this into a netcat-like functionality. It will print whatever it receives
+    over the socket and send everything you type.
+
+    Optional parameters are:
+        delimeter - Specify a delimeter to be appended to all data sent over the socket. Defaults to a '\n'
+    '''
+    def interact(self, delimiter="\n"):
+        try:
+            r = self.recieverPrinter(self.socket)
+            r.daemon = True # ensure the thread quits when the main thread dies
+            r.start() # start the reciever thread
+            # enter the send loop
+            while True:
+                command = raw_input()
+                if command == "exit":
+                    # die in a pretty manner
+                    r.kill()
+                    self.socket.sendall(command+delimiter)
+                    return
+                self.socket.sendall(command+delimiter)
+        except KeyboardInterrupt:
+            r.kill()
+            return
+        except:
+            pass
+
+    def settimeout(self, timeout):
+        self.timeout = timeout
+
+    def start(self, connection_data):
+        self.socket = socket.socket()
+        self.socket.connect(connection_data)
+
+    def shutdown(self):
+        self.socket.close()
+        self.socket = None
+                        
+    class recieverPrinter(threading.Thread):
+        def __init__(self, socket):
+            super(nexpect.recieverPrinter, self).__init__()
+            self.socket = socket
+            self.socket.settimeout(0.5)
+            self.stop = False
+        def run(self):
+            while not self.stop:
+                try:
+                    sys.stdout.write(self.socket.recv(1024))
+                    sys.stdout.flush()
+                except:
+                    pass
+        def kill(self):
+            self.stop = True
+
+
+class TimeoutException(Exception): 
+    pass
+
+    
---
+++ test_nexpect.py
@@ -0,0 +1,71 @@
+import unittest, thread, socket, nexpect, sys, time
+
+class nexpectTest(unittest.TestCase):
+
+    def startTestServer(self):
+        try:
+            server = socket.socket()
+            server.bind(('0.0.0.0',9000))
+            server.listen(1)
+            while True:
+                sock,addr = server.accept()
+                thread.start_new_thread(self.handler,(sock,))
+        except Exception as e:
+            print e.message
+
+    def handler(self, sock):
+        try:
+            data = ''
+            while True:
+                data = sock.recv(1024)
+                if 'exit' in data:
+                    return
+                elif 'test_single_expect' in data:
+                    sock.sendall('This is only a test.\n\n BOB')
+                elif 'test_multi_expect' in data:
+                    sock.sendall('This is only a test.\n\n Steve')
+                elif 'Timeout' in data:
+                    time.sleep(2)
+        except:
+            return
+
+    def setUp(self):
+        thread.start_new_thread(self.startTestServer,())
+
+    def test_createModuleWithSocket(self):
+        sock = socket.socket()
+        sock.connect(('localhost',9000))
+        s = nexpect.nexpect(sock)
+        self.assertEqual(s.timeout, 30)
+        s.sendline('exit')
+        s.shutdown()
+        
+        s = nexpect.spawn(('localhost',9000))
+        self.assertEqual(s.timeout, 30)
+        s.sendline('exit')
+        s.shutdown()
+        
+        self.assertRaises(TypeError, nexpect.spawn, ('123'))
+
+    def test_expect(self):
+        s = nexpect.spawn(('localhost',9000))
+        s.sendline('test_single_expect')
+        returned = s.expect('B.B')
+        shouldMatch = 'This is only a test.\n\n BOB'
+        self.assertEqual(returned, shouldMatch)
+        
+        s.sendline('test_multi_expect')
+        returned,index = s.expect(['B.B','Steve'])
+        shouldMatch = ('This is only a test.\n\n Steve',1)
+        self.assertEqual(shouldMatch[0], returned)
+        self.assertEqual(shouldMatch[1], index)
+        
+        s.sendline('Timeout')
+        with self.assertRaises(nexpect.TimeoutException):
+            s.expect('timeout',timeout=1)
+        s.sendline('exit')
+        s.shutdown()
+
+if __name__ == '__main__':
+    sys.argv.append('-v')
+    unittest.main()
